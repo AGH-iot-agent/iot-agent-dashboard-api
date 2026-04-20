df711242f70d4bf41af41eac92c976ce328fcc12
dominiks01 <dominikszot950@gmail.com>
2026-04-21 01:04:48 +0200

    Update infra

--- src/main/java/io/agh/iot/dashboard/config/SecurityConfig.java
+++ src/main/java/io/agh/iot/dashboard/config/SecurityConfig.java
@@ -3,8 +3,8 @@ package io.agh.iot.dashboard.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
